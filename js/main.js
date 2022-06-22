735c04596e0cb6129aca69a104fbcb3b5f0f09f0
Konstantin <konkomen1@gmail.com>
2022-06-22 17:26:08 +0500

    Новая версия функции 0.1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// function getRandomNumber(minNumber, maxNumber) {
-//   if (typeof (minNumber && maxNumber) !== 'boolean') {
-
-//     if (minNumber > maxNumber) {
-//       throw Error ('Не верный диапозон.');
-//     }
-
-//     if (minNumber >= 0 && maxNumber >= 0) {
-//       const randomInteger = minNumber + Math.random() * (maxNumber + 1 - minNumber);
-//     }
-
-//     return console.log(Math.floor(randomInteger));
-//   }
-
-//   throw Error ('Не верные аргументы.');
-// }
-
-
 function getRandomNumber (minNumber, maxNumber) {
   const lower = Math.ceil(Math.abs(minNumber));
   const upper = Math.floor(Math.abs(maxNumber));
@@ -24,17 +6,102 @@ function getRandomNumber (minNumber, maxNumber) {
     throw Error ('Неверный диапозон. Min должен быть меньше Max');
   }
 
-
   const randomInteger = lower + Math.random() * (upper + 1 - lower);
-  return console.log(Math.floor(randomInteger));
+  return Math.floor(randomInteger);
 }
 
-
-getRandomNumber(33, 65);
-
+getRandomNumber(0 , 25);
 
 function getLenghtComment(comment, maxLenght) {
   return comment.lenght <= maxLenght;
 }
 
 getLenghtComment(33, 140);
+
+const NAMES = [
+  'Александра',
+  'Вера',
+  'Вероника',
+  'Антонина',
+  'Павел',
+  'Анатолий',
+  'Виктория',
+  'Лев',
+  'Николай',
+  'Алия',
+  'Полина',
+  'Ева',
+  'Александр',
+  'Татьяна',
+  'Владимир',
+  'Николь',
+  'Артём',
+  'Алексей',
+  'Василиса',
+  'Михаил',
+  'Андрей',
+  'Никита',
+  'Мария',
+  'Максим',
+  'София',
+];
+
+const DESCRIPTION_FOTO = [
+  'Самая известная фотография прошлого',
+  'Модельный снимок',
+  'Вид изобразительного искусства',
+  'Романтическая фотография',
+  'Фото на паспорт',
+  'Нет на фотографии – нет проблемы',
+  'Композиция в фотографии',
+  'Ава без лица',
+  'Черный волк',
+  'Красивое фото леса',
+  'Самая красивая кошка',
+  'Перед нами большой экспонат',
+  'Девушка делает покупки',
+  'Красивое пожелтевшее дерево на краю обрыва',
+  'Самый лучший товар за свои деньги',
+  'Самая быстрая и дорогая машина в мире',
+  'Синее безоблочное небо',
+  'Беседка с крышей',
+  'Словарь Ожегова',
+  'Киногерой вселенной марвел Deadpool',
+  'Объявление о вакансии на работу frontend разработчика',
+  'Красивый космос, с множеством звезд',
+  'Упавшая капля в воду',
+  'Бегущий по снегу тигр',
+  'Серый кот с голубыми глазами, лежит на диване',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTION_USER = 25;
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+function createUser() {
+  return ({
+    id: '',
+    url: '',
+    description: getRandomArrayElement(DESCRIPTION_FOTO),
+    likes: getRandomNumber(15, 200),
+    comments: [
+      {
+        id: '',
+        avatar: 'img/avatar-{{getRandomNumber(1, 6)}}.svg',
+        message: getRandomArrayElement(MESSAGES),
+        name: getRandomArrayElement(NAMES),
+      }
+    ],
+  });
+}
+
+const similarUsers = Array.from({length: DESCRIPTION_USER}, createUser);
